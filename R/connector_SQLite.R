f3a5881e1cf0eaf527674adcbc429793295fcec3
Damien Dotta <damien.dotta@live.fr>
2025-05-06 16:04:12 +0200

    doc: ajout slide sur l'utilisation avec les BDD SQLite

--- R/connector_SQLite.R
+++ R/connector_SQLite.R
@@ -1,28 +1,26 @@
 sqlLite_objects <- list(
-
+  
   connect_function = function(user,pw){
-    con_sqlite <- dbConnect(RSQLite::SQLite(), ":memory:")
-    dbWriteTable(con_sqlite, "mtcars", mtcars)
-    dbWriteTable(con_sqlite, "CO2", data.frame(CO2))
-    dbWriteTable(con_sqlite, "billboard", data.frame(tidyr::billboard))
-    return(con_sqlite)
+    chemin_IPPAP_dev <- "~/CERISE/02-Espace-de-Production/090_Prix/9010_IPPAP/IPPAP_app/dev/App/donnees/IPPAP.sqlite"
+    db_IPPAP <- dbConnect(RSQLite::SQLite(), dbname = chemin_IPPAP_dev)
+    return(db_IPPAP)
   },
-
+  
   req_login = FALSE,
-
+  
   list_schemas_function = function(con){
     return('default')
   },
-
+  
   list_tables_function = function(con,dbname){
     tables <- dbGetQuery(con, "SELECT name FROM sqlite_master WHERE type='table'")
     return(tables)
   },
-
+  
   remote_table_function = function(con,dbname,tablename){
     lz <- dplyr::tbl(con, tablename)
   }
-
+  
 )
 
 connectors <- list("SQLite DB"=sqlLite_objects)
